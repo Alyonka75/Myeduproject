20205a7aa77fb01ed83df63e45a893ce06b71b41
Andrey Mozgolin <andrey.mozgolin@outlook.com>
2024-04-20 17:48:36 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,7 @@
+URL_SERVICE = "https://54f9f061-88a6-46f4-a5cb-bcd584ff4bcc.serverhub.praktikum-services.ru"
+
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits"
+USER_TABLE_PATH = "/api/db/resources/users.csv"
